016fed3b407d12e4496730b30daa194214f21c26
Mohammad Albai <m2d99ly@gmail.com>
2017-07-06 03:04:27 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,26 +1,19 @@
 import java.io.*;
 import org.apache.commons.codec.binary.*;
+import java.util.*;
 
 public class Main
 {
 	public static void main(String[] args) throws IOException, InterruptedException
 	{
-
-		MQl mql =  new MQl();
-		mql.CreateDataBase("test","root");
-    	mql.CreateTable(new String[]{"users","id","name","age"});
-		mql.Connect("test","root");
-		if(!mql.MQlConnected) {throw new Error(mql.MQlError);}
-		mql.UseTable("users");
-		mql.Query("id");
-		String[] data = mql.DataAsArray;
-		int index = 0;
-	//	while(index<data.length)	{  DoStuff	}
-		mql.EmptyTable();
-		mql.InsertData(new String[]{"0","root"});
-	//  mql.Reset();
-		mql.Disconnect();
-	//	System.out.print(mql.MQlConnected);
-
+		double f = new Date().getSeconds();
+		MQl m = new MQl();
+		m.Connect("test","root");
+		m.UseTable("users");
+	//	m.InsertData(new String[]{"0","mohammad","18"});
+		String[][] data = m.CollectDataTable(new String[]{"name","age"},MQlQuery.First);
+		System.out.println(data[0][0]);
+		m.Disconnect();
+		System.out.println((double)new Date().getSeconds()-f);
 	}
 }
